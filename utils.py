775272d78b74accd0393e794b40b7427281f937d
Sr0121 <532835534@qq.com>
2019-11-23 21:19:14 -0500

    fixed bugs

--- utils.py
+++ utils.py
@@ -6,7 +6,9 @@ import numpy as np
 def getData():
     trainData = pd.read_csv("./train.csv")
 
-    n = trainData.shape[0]
+    # n = trainData.shape[0]
+    n = 24
+    print(n)
     Fish = []
     Flower = []
     Gravel = []
@@ -19,26 +21,24 @@ def getData():
             print(i)
         filePath = trainData.iloc[i, 0]
         mask = trainData.iloc[i, 1]
-        if pd.isnull(mask):
-            i += 1
-            continue
-        if i < n-1 and "_" not in trainData.iloc[i+1, 0]:
-            mask = mask + trainData.iloc[i+1, 0]
-            i += 1
-
         newMask = np.zeros((1400, 2100))
-        pixels = mask.split(" ")
-        lastPixels = 0
-        for j in range(1, len(pixels), 2):
-            if len(pixels[j]) < lastPixels:
-                continue
-            start = int(pixels[j-1])
-            maskRange = int(pixels[j])
-            for k in range(maskRange):
-                xIndex = (start + k - 1) % 1400
-                yIndex = (start + k - 1) // 1400
-                newMask[xIndex][yIndex] = 1
-            lastPixels = len(pixels[j])
+        if pd.isnull(mask) == False:
+            if i < n-1 and "_" not in trainData.iloc[i+1, 0]:
+                mask = mask + trainData.iloc[i+1, 0]
+                i += 1
+
+            pixels = mask.split(" ")
+            lastPixels = 0
+            for j in range(1, len(pixels), 2):
+                if len(pixels[j]) < lastPixels:
+                    continue
+                start = int(pixels[j-1])
+                maskRange = int(pixels[j])
+                for k in range(maskRange):
+                    xIndex = (start + k - 1) % 1400
+                    yIndex = (start + k - 1) // 1400
+                    newMask[xIndex][yIndex] = 1
+                lastPixels = len(pixels[j])
 
         # if "00dec6a.jpg_Sugar" in filePath:
         #     newImage = np.zeros((1400, 2100, 3))
